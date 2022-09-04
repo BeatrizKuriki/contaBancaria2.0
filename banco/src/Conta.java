6028d383aff89a0decb07468c70f0adb3b020480
BeatrizKuriki <b.f.kuriki@gmail.com>
2022-09-03 21:47:42 -0300

    feat: created balance method

--- banco/src/Conta.java
+++ banco/src/Conta.java
@@ -1,33 +1,38 @@
 
 public class Conta {
-	double saldo;
+	private double saldo;
 	int agencia;
 	int numero;
-	String titular;
-	
-	public void deposito(double valor){
+	TitularConta titular;
+
+	public void deposito(double valor) {
 		this.saldo = this.saldo + valor;
-		
+
 	}
-	
+
 	public boolean sacar(double valor) {
-		if(this.saldo >= valor) {
-			this.saldo = this.saldo -valor;
+		if (this.saldo >= valor) {
+			this.saldo = this.saldo - valor;
 			return true;
-		}else {
+		} else {
 			return false;
 		}
-		
+
 	}
-	
+
 	public boolean transferencia(double valor, Conta destinataria) {
-		if(this.saldo>= valor) {
+		if (this.saldo >= valor) {
 			this.saldo -= valor;
 			destinataria.deposito(valor);
 			return true;
-		}else {
+		} else {
 			return false;
 		}
+
+	}
+
+	public double pegaSaldo() {
+		return this.saldo;
 	}
 	
 
