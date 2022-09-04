ea0b31188f8ad2b8b41b6f51b0ffdd65afa081b6
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-09-04 09:11:29 -0300

    feat: created get and set method

--- banco/src/Conta.java
+++ banco/src/Conta.java
@@ -1,9 +1,9 @@
 
 public class Conta {
 	private double saldo;
-	int agencia;
-	int numero;
-	TitularConta titular;
+	private int agencia;
+	private int numero;
+	private TitularConta titular;
 
 	public void deposito(double valor) {
 		this.saldo = this.saldo + valor;
@@ -31,11 +31,24 @@ public class Conta {
 
 	}
 
-	public double pegaSaldo() {
+	public double getSaldo() {
 		return this.saldo;
 	}
 	
+	public int getNumero() {
+		return this.numero;
+		
+	}
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
 
-	
-	
+	public  void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	 public int getAgencia() {
+		 return this.agencia;	
+
+}
 }
