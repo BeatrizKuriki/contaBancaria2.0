dba27b4b46ea1f2e7a96f080b407cd92da965e2b
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-09-04 09:39:18 -0300

    feat: created constructor of Conta Class

--- banco/src/Conta.java
+++ banco/src/Conta.java
@@ -4,6 +4,17 @@ public class Conta {
 	private int agencia;
 	private int numero;
 	private TitularConta titular;
+	
+	//construtor padrao
+	public Conta() {
+		
+	}
+	
+	public Conta(int agencia, int numero) {
+		this.agencia = agencia;
+		this.numero = numero;
+		
+	}
 
 	public void deposito(double valor) {
 		this.saldo = this.saldo + valor;
@@ -41,14 +52,29 @@ public class Conta {
 	}
 	
 	public void setNumero(int numero) {
+		if(numero <= 0) {
+			System.out.println("NÚMERO DA CONTA DEVE SER POSITVO E MAIOR QUE ZERO!");
+		return;
+		}
 		this.numero = numero;
 	}
 
 	public  void setAgencia(int agencia) {
+		if(agencia<= 0) {
+			System.out.println("NÚMERO DE AGÊNCIA DEVE SER POSITVO E MAIOR QUE ZERO!");
+		return;
+		}
 		this.agencia = agencia;
 	}
 	 public int getAgencia() {
 		 return this.agencia;	
 
 }
+	 public void setTitular(TitularConta titular) {
+		this.titular = titular;
+	}
+	 
+	 public TitularConta getTitular() {
+		return titular;
+	}
 }
